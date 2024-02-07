6b2352ea103ff5439dcb88ba25c74f549d08c99b
A412787 <alberto.bollino@enel.com>
2024-02-07 15:52:28 +0100

    added spinner in get list call

--- patient-app/src/pages/PatientList.js
+++ patient-app/src/pages/PatientList.js
@@ -3,19 +3,23 @@ import axios from "axios";
 import GridComponent from "../components/gridComponent";
 import patientTableColumns from "../models/patientTableColumns";
 import endpoints from "../endpoints/endpoints";
+import Spinner from 'react-bootstrap/Spinner';
 
 function PatientList() {
   const columns = patientTableColumns;
   const [data, setData] = useState([]);
+  const [showSpinner, setShowSpinner] = useState();
 
   useEffect(() => {
     getPatientList();
   }, []);
 
   const getPatientList = async () => {
+    setShowSpinner(true);
     const result = await axios.get(endpoints.GET_LIST, {
       auth: endpoints.AUTH,
     });
+    setShowSpinner(false);
     setData(result.data);
   };
 
@@ -25,16 +29,24 @@ function PatientList() {
     }
   };
 
-  return (
-    <div className="px-5 d-flex flex-column justify-content-center">
-      <h2 className="my-5">Patients List</h2>
-      <GridComponent
-        columns={columns}
-        data={data}
-        sendDataToParent={handleDataFromChild}
-      />
-    </div>
-  );
+  if(!showSpinner){
+    return (
+      <div className="px-5 d-flex flex-column justify-content-center">
+        <h2 className="my-5">Patients List</h2>
+        <GridComponent
+          columns={columns}
+          data={data}
+          sendDataToParent={handleDataFromChild}
+        />
+      </div>
+    );
+  } else {
+    return (
+      <div className="spinner">
+        <Spinner animation="border" variant="dark"/>
+      </div>
+    )
+  }
 }
 
 export default PatientList;
